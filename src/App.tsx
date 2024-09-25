9a66eece435df5eda8998f5dd6eb18f8ffcf869d
araheja <araheja@umich.edu>
2024-09-24 20:41:01 -0400

    started lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <header className="header">
+        <nav className="nav-links">
+          <a href="mailto:venn-michigan@umich.edu">Contact Us</a>
+        </nav>
+      </header>
+      <main className="main-content">
+        <h1 className="brand-name">Venn</h1>
+        <br/>
+        <br/>
+        <button className="primary-button" onClick={() => window.location.href = 'https://tally.so/r/3lWGqV'}>
+          Join our Waitlist
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
+      <footer className="footer">
+        <p>© 2024 Venn</p>
+
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
