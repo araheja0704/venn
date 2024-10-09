cb8eb7107815c961da838ffa02e6fbb4d8e15e18
araheja <araheja@umich.edu>
2024-10-09 15:51:08 -0400

    added insta

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import './css/App.css';
 import BubbleBackground from './js/components/Bubbles';
 import Logo from './js/components/Logo';
 import Button from './js/components/Button';
+import InstagramIcon from './js/components/InstagramIcon';
 
-
+function Navbar() {
+  return (
+      <div className="navbar">
+          <div className="navbar-left">
+          <span className="navbar-title">venn</span>
+          </div>
+          <div className="navbar-right">
+              <InstagramIcon />
+          </div>
+      </div>
+  );
+}
 
 function App() {
     return (
       <>
         <BubbleBackground />
+        <Navbar /> 
         <div className="wrapper">
         <div className="main-content">
           <Logo />
---
+++ src/js/components/InstagramIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../css/App.css';
+
+function InstagramIcon() {
+    return (
+        <a href="https://www.instagram.com/vennatmichigan/" target="_blank" rel="noopener noreferrer" className="instagram-icon">
+            <i className="fab fa-instagram"></i>
+        </a>
+    );
+}
+
+export default InstagramIcon;
