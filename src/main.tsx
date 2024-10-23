348770465db52141b6043658af63a802845df059
araheja <araheja@umich.edu>
2024-10-23 14:16:09 -0400

    tried url change

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 
